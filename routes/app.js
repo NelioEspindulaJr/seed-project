5f0e8be25f50b14deacf44428d9aae69432b3e68
Nélio Espíndula Junior <nelio@superticket.com.br>
2023-10-02 22:29:36 -0300

    parte III b - pagina 27
    Signed-off-by: Nélio Espíndula Junior <nelio@superticket.com.br>

--- routes/app.js
+++ routes/app.js
@@ -1,22 +1,43 @@
-var express = require('express'); 
+var User = require("../models/user");
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index');
+router.get("/node-mongodb-mongoose-user", function (req, res, next) {
+  res.render("node");
 });
 
-router.get('/message', function (req, res, next) {
-    res.render('node');
+router.post("/node-mongodb-mongoose-user", async function (req, res, next) {
+  var emailVar = req.body.emailBody;
+  var userObject = new User({
+    firstName: "Vinicius",
+    lastName: "Rosalen",
+    password: "password",
+    email: emailVar,
+  });
+  await userObject.save();
+
+  res.redirect("/node-mongodb-mongoose-user-busca");
 });
 
-router.get('/message/:msgParam', function (req, res, next) {
-    res.render('node', {message: req.params.msgParam});
+router.get("/node-mongodb-mongoose-user", async function (req, res, next) {
+  // User.findOne({}, function (err, documents) {
+  //   if (err) {
+  //     return res.send("Error!! :-(");
+  //   }
+  //   res.render("node", { firstNameV: documents });
+  // });
+  const userFind = await User.findOne({});
+  if (isNull(userFind)) {
+    return res.send("Error!!!");
+  }
+
+  res.render("node", {
+    firstNameV: userFind.firstName,
+    lastNameV: userFind.lastName,
+    passwordV: userFind.password,
+    emailV: userFind.email,
+    messagesV: userFind.message,
+  });
 });
 
-router.post('/message', function (req, res, next) {
-    var messageVariable = req.body.messageBody;
-    res.redirect('/message/' + messageVariable);
-});
-
-
-module.exports = router; 
+module.exports = router;
