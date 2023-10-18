f2c7091ab5789879f2dcd6995e39fb7aa85e4d69
Nélio Espíndula Junior <nelio@superticket.com.br>
2023-10-18 10:18:45 -0300

    Aula-02 - MEAN Stack - parte V - a -> pagina 17
    Signed-off-by: Nélio Espíndula Junior <nelio@superticket.com.br>

--- assets/app/messages/message.services.ts
+++ assets/app/messages/message.services.ts
@@ -1,11 +1,19 @@
+import { Injectable } from "@angular/core";
 import { Message } from "./message.model";
+import { Http } from "@angular/http";
 
+@Injectable()
 export class MessageService {
+  constructor(private http: Http) {}
+
   private messageSService: Message[] = [];
 
   addMessage(message: Message) {
     this.messageSService.push(message);
     console.log(this.messageSService);
+
+    const bodyReq = JSON.stringify(message);
+    return this.http.post("http://localhost:3000/message", bodyReq);
   }
 
   getMessages() {
